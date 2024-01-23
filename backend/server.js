8324afea538f7313ae402b525526e39dfe934fc8
raahir80 <raahir80@gmail.com>
2024-01-23 16:03:57 +0530

    Initial coomit

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const userRoute = require("./routes/userRoute")
+const errorHandler =require("./middleWare/errorMiddleware")
 
 const app = express()
 
 const PORT = process.env.PORT || 5000;
 
+// Middlewares 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
+// Routes Middleware
+app.use("/api/users",userRoute)
+
+//Routes
+app.get("/",(req,res) => {
+    res.send("Home Page");
+})
+
+
+// Error Middleware
+app.use(errorHandler);
 
 // connect to Db and start server
 
